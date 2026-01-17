26ba2daa82827c043b46553cc0b9fe22f0ec98a4
Explore114 <19877809391@163.com>
2026-01-17 18:07:16 +0800

    好烦，Json有错误

--- js/rules_direct.js
+++ js/rules_direct.js
@@ -2,6 +2,5 @@ window.RULES_DIRECT = {
     "/duoco": {
         "url": "https://duoco.hinincs.top/",
         "expired_at": ""
-    },
-
+    }
 };
--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2,5 +2,5 @@ window.RULES_INTERMEDIATE = {
     "/2580fug": {
         "url": "https://www.dgtwj2.shop/v2/login.php?ut=n72oo&token=aHZwdnswIyF6d2xvYmgydxEQExAGSUNDH1NZXFVQXhEUcTIc",
         "expired_at": ""
-    },
+    }
 };
