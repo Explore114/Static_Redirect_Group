c03d784958b7642e82f7b8a7c81cdd39e046f569
Shi Xiongyue <19877809391@163.com>
2026-03-15 15:27:56 +0800

    Add short link: bilib

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2,5 +2,9 @@ window.RULES_INTERMEDIATE = {
     "/baidu": {
         "url": "https://www.baidu.com/",
         "expired_at": "2026-03-16T07:26:00.000Z"
+    },
+    "/bilib": {
+        "url": "https://www.bilibili.com/",
+        "expired_at": "2026-03-16T07:27:00.000Z"
     }
 };
