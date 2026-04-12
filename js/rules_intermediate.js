638153172f343c1f5bd13a9b2e988786289ca408
Shi Xiongyue <19877809391@163.com>
2026-04-12 12:28:13 +0800

    Add short link: baidu2

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -6,5 +6,9 @@ window.RULES_INTERMEDIATE = {
     "/bilib": {
         "url": "https://www.bilibili.com/",
         "expired_at": "2026-03-16T07:27:00.000Z"
+    },
+    "/baidu2": {
+        "url": "https://www.baidu.com/",
+        "expired_at": "2026-04-13T04:27:00.000Z"
     }
 };
